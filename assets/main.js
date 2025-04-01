803c74dd62bc1782475e964c50a44e9171fa2970
Ioann <hippihop12@gmail.com>
2025-04-01 03:03:42 +0300

    up

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const titles = document.querySelectorAll(".filter-title");
 
   titles.forEach(title => {
-      title.addEventListener("click", () => {
-          const content = title.nextElementSibling;
-          content.style.display = content.style.display === "block" ? "none" : "block";
-      });
+    title.addEventListener("click", () => {
+      const content = title.nextElementSibling;
+      
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        content.style.opacity = "0";
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.opacity = "1";
+      }
+    });
   });
 });
 
